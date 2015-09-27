e1637739999320c7dd5391615d725aa60eeb8d21
Devansh Gupta <devansh@wearableintelligence.com>
2015-09-27 14:52:11 -0700

    companies path

--- app/controllers/base_company_controller.rb
+++ app/controllers/base_company_controller.rb
@@ -1,7 +1,7 @@
 class BaseCompanyController < ApplicationController
   def check_and_run
     begin
-      @company = Company.find((params[:company_id] || params[:id]).to_i)
+      @company = Company.find(company_id)
       if @user.has_company? @company
         yield
       else
@@ -10,6 +10,10 @@ class BaseCompanyController < ApplicationController
     rescue Exception => e
       redirect_error(redirect_path, :not_found, e.message)
     end
+  end
+
+  def company_id
+    (params[:company_id] || params[:id]).to_i
   end
 
   def redirect_path
